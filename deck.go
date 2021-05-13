269d8385fb98e1e9ad00644492cf5e71206baeea
Patcharapon Wangtiyong <patcharapon@igeargeek.com>
2021-05-13 18:12:04 +0700

    [Edited] section 2

--- deck.go
+++ deck.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamond", "Heart", "Clubs"}
+	cardNumber := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nice", "Ten", "Jack", "Queen", "King"}
+	for _, suits := range cardSuits {
+		for _, cNumber := range cardNumber {
+			nametest := cNumber + " of " + suits
+			cards = append(cards, nametest)
+		}
+	}
+	return cards
+}
 
 func (d deck) print() {
 	fmt.Println(d)
@@ -10,4 +27,21 @@ func (d deck) print() {
 
 func (d deck) getTitle() deck {
 	return d
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ", ")
+}
+
+func (d deck) saveFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0777)
+}
+
+func (d deck) newDeckFromFile(filename string) []string {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error : ", err)
+	}
+
+	return strings.Split(string(content), ", ")
 }
--- main.go
+++ main.go
@@ -1,28 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
 var card = "Ace of Spades"
 
-func main() {
-	cards := deck{}
-	// var card string = "Ace of Spades"
-	// for _, card := range cards {
-	// 	fmt.Println(card)
-	// 	cards.print()
-	// }
-
-	cardSuits := []string{"Spades", "Diamond", "Heart", "Clubs"}
-	cardNumber := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nice", "Ten", "Jack", "Queen", "King"}
-	for _, suits := range cardSuits {
-		for _, cNumber := range cardNumber {
-			nametest := cNumber + " of " + suits
-			cards = append(cards, nametest)
-		}
-	}
-	cards.print()
-	// fmt.Println(cards)
-
+func deal(cards deck, handSize int) (deck, deck) {
+	return cards[:handSize], cards[handSize:]
 }
 
-func newCard() string {
-	return "2 of Spades"
+func main() {
+	cards := newDeck()
+	// hand, remainingCards := deal(cards, 2)
+	// hand.print()
+	// remainingCards.print()
+	ca := cards.newDeckFromFile("test.txt")
+	// fmt.Println(ca)
+	fmt.Println(ca)
+	// if err := cards.saveFile("test.txt"); err != nil {
+	// 	fmt.Println("Error : ", err)
+	// }
+	// cards.print()
 }
